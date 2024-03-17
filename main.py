aadfeaa8e8b9a7a28faf4354e8ecfeee16e95820
JackTench <79285604+JackTench@users.noreply.github.com>
2024-03-17 17:18:15 +0000

    Created bot. Replies to /10man command.

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
 import os
 
 import discord
-import dotenv
+from discord.ext import commands
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+discord_token = os.environ.get("DISCORD_TOKEN")
+
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+
+bot = commands.Bot(command_prefix = '/', intents = intents)
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user} (ID: {bot.user.id})")
+
+@bot.command(name = "10man")
+async def start_10man(ctx):
+    await ctx.send("Starting 10 man... React with an emote to register for the 10 man.")
+
+bot.run(discord_token)
